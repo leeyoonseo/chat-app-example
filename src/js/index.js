2d18cb461d520a7103ce2683ca7a91cc226b5e75
unknown <okayoon.lee@gmail.com>
2020-01-30 22:39:14 +0900

    레이어 팝업 관련 파일 추가 및 수정 중

--- src/js/index.js
+++ src/js/index.js
@@ -14,24 +14,31 @@
 
 
     function handlerConnect(){
+
+        // [TODO] 외부로 분리
+        const content = '<input id="getUserName" type="text" placeholder="닉네임을 입력해주세요.">';
         const options = {
+            title : 'chat-app',
+            content : content,
             customButton : true,
             customButtonData : [
                 {
                     type : 'button',
                     label : '접속',
                     event : function(){
-                        const {content} = this.LayerPopup;
-                        console.log('접속');
+                        const getUserName = document.getElementById('getUserName').value;
+                        socket.emit('newUserConnect', getUserName);
+                        this.LayerPopup.close();
                     }
                 },
                 {
                     type : 'button',
                     label : '나가기',
                     event : function(){
-                        const {content} = this.LayerPopup;
                         console.log('나가기');
-                        
+                        this.LayerPopup.close();
+
+                        // [TODO] 소켓 종료 시키기
                     }
                 }
             ]
@@ -40,9 +47,8 @@
         const getNamePopup = new LayerPopup(options);
         getNamePopup.open();
 
-        var name = prompt('대화명을 입력해주세요.', '');
-
-        socket.emit('newUserConnect', name);
+        // var name = prompt('대화명을 입력해주세요.', '');
+        // socket.emit('newUserConnect', name);
     }
 
     function handlerUpdateMessage(data){
